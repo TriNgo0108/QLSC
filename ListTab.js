bf438e895054c44f3037787a712b612045d4a965
Ngo Tri <tringo0108@gmail.com>
2021-06-19 16:38:49 +0700

    add pagination

--- ListTab.js
+++ ListTab.js
@@ -19,10 +19,7 @@ export default function ListTab() {
   const [fromDate, setFromDate] = useState(currentDate);
   const [toDate, setToDate] = useState(previousMonth);
   const [reports, setReports] = useState([]);
-  const [page,setPage] = useState(1);
-  const [incidentObjects,setIncidentObjet] = useState([]);
-  const [reportStatusList,setReportStatus] = useState([]);
-  const [reportTypeList,setReportType] = useState([]);
+  const [page, setPage] = useState(1);
   useEffect(() => {
     async function getData() {
       let accessToken = await AsyncStorageStatic.getItem("@accessToken");
@@ -39,21 +36,25 @@ export default function ListTab() {
       );
       if (response_reports.status === 200) {
         reportList = response_reports.data.data.data;
-        setReports(response_reports.data.data.data);
       }
-      let response = await axios.post(
-        "https://qlsc.maysoft.io/server/api/getCommon",
-        { groups: "incidentObject, reportStatus, reportType" },
-        { headers: { Authorization: auth } }
-      );
-      if (response.status === 200) {
-        incidentObject = response.data.data.incidentObject;
-        reportStatus = response.data.data.reportStatus;
-        reportType = response.data.data.reportType;
-        setIncidentObjet(response.data.data.incidentObject);
-        setReportStatus(response.data.data.reportStatus);
-        setReportType(response.data.data.reportType);
+      
+      if (incidentObject.length === 0 || reportStatus.length === 0 || reportType.length === 0) {
+        let response = await axios.post(
+          "https://qlsc.maysoft.io/server/api/getCommon",
+          { groups: "incidentObject, reportStatus, reportType" },
+          { headers: { Authorization: auth } }
+        );
+        if (response.status === 200) {
+          incidentObject = response.data.data.incidentObject;
+          reportStatus = response.data.data.reportStatus;
+          reportType = response.data.data.reportType;
+          // setIncidentObjet(response.data.data.incidentObject);
+          // setReportStatus(response.data.data.reportStatus);
+          // setReportType(response.data.data.reportType);
+          
+        }
       }
+
       reportList.forEach((report) => {
         let status = reportStatus.filter(
           (statusCode) => statusCode.code == report.status
@@ -68,53 +69,16 @@ export default function ListTab() {
         );
         report.reportType = reportName[0].name;
       });
-
-      // let response_department = await axios.post(
-      //   "https://qlsc.maysoft.io/server/api/getAllDepartments",
-      //   {},
-      //   { headers: { Authorization: auth } }
-      // );
-
-      // if (response_department.status === 200) {
-      //   departments = response_department.data.data.data;
-      // }
+      let newReportList = [...reports,...reportList];
+      console.log(newReportList)
+      setReports(newReportList);
     }
     getData();
     // with [] parameter, just run the callback when the component mount for the first time.
     // In addition, using [] we'll tell useEffect that there are no properties we want to watch ann run the callback
     // Large different too between [] parameter and no parameter. With No Parameter, it should run when the component mounts and then on every single update
-  }, []);
-  const paging = async()=>{
-    let accessToken = await AsyncStorageStatic.getItem("@accessToken");
-    let tokenType = await AsyncStorageStatic.getItem("@tokenType");
-    let auth = `${tokenType} ${accessToken}`;
-      let response = await axios.post(
-        "https://qlsc.maysoft.io/server/api/getAllReports",
-        { page: page },
-        { headers: { Authorization: auth } }
-      );
-      let reportList = response.data.data.data;
-      let incidentObject = incidentObjects;
-      let reportStatus = reportStatusList;
-      let reportType = reportTypeList;
-      reportList.forEach((report)=>{
-        let status = reportStatus.filter(
-          (statusCode) => statusCode.code == report.status
-        );
-        report.status = status[0].name;
-        let incidentName = incidentObject.filter(
-          (incident) => incident.code == report.incidentObject
-        );
-        report.incidentObject = incidentName[0].name;
-        let reportName = reportType.filter(
-          (name) => name.code == report.reportType
-        );
-        report.reportType = reportName[0].name;
-      });
-      let newReportList = [...reports];
-      newReportList.concat(reportList)
-  }
- 
+  }, [page]);
+
   return (
     <SafeAreaView style={[styles.main, styles.column]}>
       <View style={[styles.header, styles.row]}>
@@ -137,10 +101,8 @@ export default function ListTab() {
             renderItem={Item}
             keyExtractor={(report) => report.id}
             onEndReachedThreshold={0}
-            onEndReached ={(number)=>{
-              console.log("\n");
-              setPage(page+1);
-              paging();
+            onEndReached={(number) => {
+              setPage(page + 1);
             }}
           />
         </View>
@@ -165,4 +127,3 @@ export default function ListTab() {
     </SafeAreaView>
   );
 }
-
